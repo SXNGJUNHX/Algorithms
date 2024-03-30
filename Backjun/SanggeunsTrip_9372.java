656e86dd76eddbbdf2ccb68110cc2231a180bcd8
SXNGJUNHX <untitlednamenull@naver.com>
2024-03-31 00:34:59 +0900

    update

---
+++ Backjun/SanggeunsTrip_9372.java
@@ -0,0 +1,23 @@
+package Backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SanggeunsTrip_9372 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(reader.readLine());
+
+        int t = Integer.parseInt(st.nextToken()); // 테스트 케이스
+
+        for(int i=0; i<t; i++){
+
+        }
+    }
+}
